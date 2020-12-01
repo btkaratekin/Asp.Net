50e3348a8eb7c869674b89c752c58a6bbd300960
İsmail Karaman <ismailkaraman@msn.com>
2020-12-01 10:47:25 +0300

    Add .gitignore and .gitattributes.

---
+++ OgrIsler/OgrIsler.Entities/Concrete/OgrBilgi.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OgrIsler.Entities.Concrete
+{
+    public class OgrBilgi
+    {
+        public string Ogrno { get; set; }
+
+    }
+}
